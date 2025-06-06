bb98903926ec8940f344af58326fb5e2867a9fe9
Youngone Lee <youngone.lee@accenture.com>
2025-06-05 23:42:13 -0400

    hosted on github with routing using HASHTAG like a fricken loser

---
+++ src/pages/MissingPage.jsx
@@ -0,0 +1,7 @@
+export default function MissingPage() {
+    return(
+        <div>
+            <h1> Catch all </h1>
+        </div>
+    )
+}
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+    console.log("rendered the profile page")
+    return(
+        <div>
+            <h1> Profile Page </h1>
+        </div>
+    )
+}
